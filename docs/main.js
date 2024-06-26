8f158ab26a84fd039c9d743787871d5668876430
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-06-26 22:29:27 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,5 @@
 import {dayName} from "./dayname.js";
 import seasonNames from "./seasonname.js";
-import {parse} from "ini";
 
 let now = new Date();
 console.log(`Today is ${dayName(now.getDay())}`);
@@ -8,5 +7,3 @@ console.log(`Today is ${dayName(now.getDay())}`);
 
 console.log(seasonNames);
 
-console.log(parse("x = 10\ny = 20"));
-// → {x: "10", y: "20"}
