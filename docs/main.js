898ec032a3fee9d69fa52055d47999db3956faca
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2024-06-26 22:11:29 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,7 @@
 import {dayName} from "./dayname.js";
+import seasonNames from "./seasonname.js";
 let now = new Date();
 console.log(`Today is ${dayName(now.getDay())}`);
 // → Today is Monday
 
-document.writeln("hello world");
+console.log(seasonNames);
